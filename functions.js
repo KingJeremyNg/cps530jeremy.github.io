2f33fe4d92ad6fac20c8847e2a746d7b3b6de00e
KingJeremyNg <jeremy97ng@gmail.com>
2019-09-22 14:25:15 -0400

    Changed login for github pages

--- functions.js
+++ functions.js
@@ -1,11 +1,12 @@
-function validateForm() {
-    var x = document.forms["login"]["uname"].value;
-    var y = document.forms["login"]["psw"].value;
-    if (x == "jeremyng" && y == "jeremyng") {
-        return true;
+function checkAuth() {
+    let usernameInput = document.querySelector('#username');
+    let passwordInput = document.querySelector('#password');
+    if(usernameInput.value == "jeremyng" && passwordInput.value == "jeremyng") {
+        alert("Successfully Logged in: Redirecting");
+        window.location.assign("main.html");
+    } else {
+        alert("Wrong Username or Password!");
+        usernameInput.value = "";
+        passwordInput.value = "";
     }
-    else {
-        alert("Failed to login");
-        return false;
-    }
-};
+}
